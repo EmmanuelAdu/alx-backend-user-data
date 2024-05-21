9871d08d017fa3dbe5c3253daadce0a17d822c01
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-21 16:28:13 +0000

    Correct all pycodestyle errors in `auth` file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,22 @@ class Auth:
         Returns:
             bool: True if the path requires authentication, False otherwise.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
