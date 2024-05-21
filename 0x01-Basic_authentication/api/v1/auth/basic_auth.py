0cff11b3b1332548182114e6018713e3dea36dca
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-21 17:39:52 +0000

    Include Basic Auth module for new environment AUTH

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for authentication using Basic Auth
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class for authentication using Basic Auth
+    """
+    pass
