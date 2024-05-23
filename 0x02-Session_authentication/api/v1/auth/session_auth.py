92224a051bfe96f906bcd12ab983215caba32760
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-23 17:46:25 +0000

    Include method to create session uuid from user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,24 @@
 '''Module for authentication using Session Auth
 '''
 
+
 from .auth import Auth
+
+
+from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Handles Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Generate a Session ID for a User ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
