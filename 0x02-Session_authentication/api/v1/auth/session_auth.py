300e9e21188f75fe435f4b661a62a17d8cf8b7d5
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-23 17:53:08 +0000

    Include method to retrieve USER ID based on SESSION ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,10 @@ class SessionAuth(Auth):
         id = uuid4()
         self.user_id_by_session_id[str(id)] = user_id
         return str(id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the USER ID based on the Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
