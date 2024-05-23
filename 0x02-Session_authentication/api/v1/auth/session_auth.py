8ed3aebd3b42719607cdfe329fd7a740237fbbe3
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-23 16:22:19 +0000

    Create a SessionAuth class to handle all session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Module for authentication using Session Auth
+'''
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Handles Session Authentication
+    """
+    pass
