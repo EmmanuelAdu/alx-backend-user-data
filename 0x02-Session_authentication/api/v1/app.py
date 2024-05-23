58068b102bdbbf39c6c46dff6aa068702faeed82
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-23 15:40:32 +0000

    Successfully fixed api error in headers authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -66,4 +66,4 @@ def forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
