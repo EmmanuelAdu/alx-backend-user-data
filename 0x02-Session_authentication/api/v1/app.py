80a430c23a150f61a25749e99fe5eb1a966e6a99
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-23 18:13:59 +0000

    Include login path as part of the excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,11 +37,13 @@ def before_request():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401, description='Unauthorized')
+        cookie = auth.session_cookie(request)
+        if auth.authorization_header(request) is None and cookie is None:
+            abort(401, description="Unauthorized")
         if auth.current_user(request) is None:
             abort(403, description='Forbidden')
 
