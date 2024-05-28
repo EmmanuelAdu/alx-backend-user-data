fc1a76e66a5ef93d082187fe5f3afde97ee230a0
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-28 15:46:15 +0000

    Include update_user method to update a users attribute

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,3 +51,14 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
