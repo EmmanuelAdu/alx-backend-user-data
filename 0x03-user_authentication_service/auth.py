4121d4c4dba1c24d8bd83d362ff231a4e60e8069
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-28 16:15:41 +0000

    Update Auth to add a register endpoint for new users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,8 @@ Auth module
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 
 def _hash_password(password: str) -> str:
@@ -13,3 +15,22 @@ def _hash_password(password: str) -> str:
     Hashes a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """
+        Register a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError("User {} already exists".format(email))
