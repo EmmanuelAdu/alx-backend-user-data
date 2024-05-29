3f7f6f0f2fe0407c37d635a885bb1f6b4928aca5
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-29 22:27:02 +0000

    Destroy a session id based on user ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,15 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy a session
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
